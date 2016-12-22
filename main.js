8905019f0f57b4424b3d38b82de1c97acaf60aa2
le-garff-yoann <pe.weeble@yahoo.fr>
2016-12-22 18:49:31 +0100

    don't update the docs

--- main.js
+++ main.js
@@ -7,37 +7,37 @@
 
 'use strict;'
 
-const _ = require('lodash');
-const joi = require('joi');
-
-const db = require('@arangodb').db;
-const graphModule = require('@arangodb/general-graph');
-const router = require('@arangodb/foxx/router')();
+const _ = require('lodash'),
+    joi = require('joi'),
+    db = require('@arangodb').db,
+    graphModule = require('@arangodb/general-graph'),
+    router = require('@arangodb/foxx/router')();
 
 router.post('/batch', function (req, res) {
-    var resBody = [];
-
-    var events = {};
+    var notifications = [],
+        errors = [],
+        events = {};
 
     for (const serializedEvent of req.body) {
         try {
             const event = JSON.parse(serializedEvent);
 
-            if (
+            if ( ! (
                 event instanceof Array &&
                 event.length == 5 &&
                 _.isInteger(event[0]) &&
                 (_.isNull(event[1]) || _.isString(event[1]) || _.isInteger(event[1])) &&
                 (_.isString(event[2]) || _.isInteger(event[2])) &&
                 (_.isNull(event[1]) || _.isString(event[1]) || _.isInteger(event[1]))
-            ) {
-                const documentCollectionName = _.isNull(event[1]) ? 'void' : 'class_' + event[1];
+            )) throw new Error('invalid event');
 
-                if ( ! _.has(events, documentCollectionName)) events[documentCollectionName] = [];
+            const documentCollectionName = _.isNull(event[1]) || ! _.isObjectLike(event[4]) ? 'void' : 'class_' + event[1];
 
-                events[documentCollectionName].push(event);
-            }
+            if ( ! _.has(events, documentCollectionName)) events[documentCollectionName] = [];
+
+            events[documentCollectionName].push(event);
         } catch (e) {
+            errors.push(e.toString());
         }
     }
 
@@ -48,63 +48,58 @@ router.post('/batch', function (req, res) {
             const documentCollection = db._collection(documentCollectionName);
 
             documentCollection.ensureSkiplist('time');
-            documentCollection.ensureUniqueSkiplist('id');
+            documentCollection.ensureSkiplist('id');
             documentCollection.ensureFulltextIndex('description');
 
-            for (const event of _.uniqBy(events[documentCollectionName], 'id')) {
-                const error = [];
+            for (const event of events[documentCollectionName]) {
+                var errors = [];
 
                 try {
-                    var cursor = documentCollection.byExample({
-                        id: event[2]
+                    documentCollection.insert({
+                        time: event[0],
+                        id: event[2],
+                        description: event[3],
+                        data: event[4]
                     });
-
-                    if (cursor.hasNext()) {
-                        documentCollection.update(cursor.next()._id, {
-                            time: event[0],
-                            description: event[3],
-                            data: event[4]
-                        });
-                    } else {
-                        documentCollection.insert({
-                            time: event[0],
-                            id: event[2],
-                            description: event[3],
-                            data: event[4]
-                        });
-                    }
                 } catch (e) {
-                    errors.push(e);
+                    errors.push(e.toString());
                 }
 
-                resBody.push({
+                notifications.push({
                     class: event[1],
                     id: event[2],
                     errors: errors
                 });
             }
         } catch (e) {
+            errors.push(e.toString());
         }
     }
 
-    res.json(resBody);
+    res.json({
+        notifications: notifications,
+        errors: errors
+    });
 }).body(
     joi.array().required(),
     'List of serialized Navel::Event'
 ).response(
-    joi.array().items(
-        joi.object({
-            class: joi.alternatives([
-                joi.string().allow(null),
-                joi.number()
-            ]),
-            id: joi.alternatives([
-                joi.string().allow(null),
-                joi.number()
-            ]),
-            errors: joi.array()
-        }).required()
-    ).required(),
+    joi.object({
+        notifications: joi.array().items(
+            joi.object({
+                class: joi.alternatives([
+                    joi.string().allow(null),
+                    joi.number()
+                ]),
+                id: joi.alternatives([
+                    joi.string().allow(null),
+                    joi.number()
+                ]),
+                errors: joi.array()
+            }).required()
+        ).required(),
+        errors: joi.array().required()
+    }).required(),
     'List of Navel::Notification constructor properties'
 );
 
